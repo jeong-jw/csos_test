c670b94e76679aa419e4f97c9a0d751aecf424f1
jeongjw <jw.jeong@sqisoft.com>
2021-07-12 17:38:33 +0900

    test

---
+++ src/main/java/com/example/csos_test/CsosTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.csos_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsosTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsosTestApplication.class, args);
+    }
+
+}
